fe5f1b23c81fdb9acef7777e5f3fdd4f77f21809
Arley Schrock <arleyschrock@outlook.com>
2017-07-25 09:21:21 -0500

    .

--- newutil.sh
+++ newutil.sh
@@ -2,7 +2,10 @@
 # Builds, tags, and pushes the dotnet-core-build image
 # If using the docker hub/store, set to your username. Otherwise,
 # set it to the registry URL (example: localhost:5000)
+
+if [! $REGISTRY ]; then
 REGISTRY=arleyschrock
+fi
 
 # adjust to modify the tag
 IMAGEID=dotnet-core-build
